141bae706717719114634042b8f36ccd67b9ae64
Trenoid <112693010+Trenoid@users.noreply.github.com>
2022-10-16 16:55:35 +0300

    Update 1zad.py

--- 1zad.py
+++ 1zad.py
@@ -1,28 +1,37 @@
 from random import randint
-chislo=0
-spisok=[]
-counter = 0
-allcounter = 0
-while chislo !="q":
-	chislo=input("Введите число:  ")
-	if chislo== "q":
-		break
-	randomchislo=randint(1,9)
-	
-	if (int(chislo) >0) and (int(chislo) <10):
-		if int(chislo)==randomchislo:
-			print("Угадано загаданное число -",randomchislo)
-			counter +=1
-		else:
-			print("Не угадано загаданое число -",randomchislo)
-		spisok.append(randomchislo)
-		print("Список загаданных чисел -"," ".join(map(str,spisok)))
-		allcounter+=1
-		ugadano=int(100*counter/allcounter)
-		neugadano=int(100*(allcounter-counter)/allcounter)
-		print("Статистика угадано/не угадано:",ugadano,"%",neugadano,"%")
-		print("")
-		print("")
-	
-	else:
-		print("Введите число от 1 до 9")
+t = int(input(" Введите t "))
+s = int(input(" Введите s "))
+a = []
+j = ""
+for m in range(t):
+    for n in range(s):
+        j = j + str(randint(0,1))
+    a.append(j)
+    j = ""
+print(a)
+k = 0
+l = ""
+mas = []
+udal = []
+gt = "False"
+for m in range(t):
+    l = a[m]
+    mas.append(a[m])
+    dlina = len(l)
+    for ch in range(dlina-1):
+        if l[ch] == "1":
+            for m in range(ch,dlina-1):
+                if l[m] == "0":
+                    for i in range(m,dlina):
+                        if l[i] == "1":
+                            gt = "true"
+                    if gt == "true":
+                        k +=1
+                    gt = "False"
+            break
+
+
+    print(k)
+    k = 0
+    l = ""
+    mas.clear()
