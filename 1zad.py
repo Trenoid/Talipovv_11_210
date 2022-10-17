fa42abf63ab7b873e96e7677d1d7748b714f7fca
Trenoid <112693010+Trenoid@users.noreply.github.com>
2022-10-17 13:11:07 +0300

    Update 1zad.py

--- 1zad.py
+++ 1zad.py
@@ -1,28 +1,23 @@
-from random import randint
-chislo=0
-spisok=[]
-counter = 0
-allcounter = 0
-while chislo !="q":
-	chislo=input("Введите число:  ")
-	if chislo== "q":
-		break
-	randomchislo=randint(1,9)
-	
-	if (int(chislo) >0) and (int(chislo) <10):
-		if int(chislo)==randomchislo:
-			print("Угадано загаданное число -",randomchislo)
-			counter +=1
-		else:
-			print("Не угадано загаданое число -",randomchislo)
-		spisok.append(randomchislo)
-		print("Список загаданных чисел -"," ".join(map(str,spisok)))
-		allcounter+=1
-		ugadano=int(100*counter/allcounter)
-		neugadano=int(100*(allcounter-counter)/allcounter)
-		print("Статистика угадано/не угадано:",ugadano,"%",neugadano,"%")
-		print("")
-		print("")
-	
-	else:
-		print("Введите число от 1 до 9")
+message = input("Введите сообщение") + "I"
+count = 1
+simvol = ""
+vivodmessage = ""
+
+
+while message != "quitI":
+
+    for i in range(len(message)-1):
+        if message[i] == message[i+1]:
+            count += 1
+            simvol = message[i]
+        if message[i] != message[i+1]:
+            if count == 1:
+                vivodmessage += message[i]
+            if count > 1:
+                vivodmessage += simvol + str(count)
+            count = 1
+
+
+    print(vivodmessage)
+    vivodmessage = ""
+    message = input("Введите сообщение") + "I"
