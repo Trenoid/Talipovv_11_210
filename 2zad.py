871c6ce486a832c95768de511309303c2b00865f
Trenoid <112693010+Trenoid@users.noreply.github.com>
2022-11-01 01:23:25 +0300

    Update 2zad.py

--- 2zad.py
+++ 2zad.py
@@ -1,56 +1,71 @@
-from random import randint
-print("")
+man = ["ов", "ев", "ин", "ын", "ский", "цкий"]
+woman = ["ова", "ева", "ина", "ына", "ская", "цкая"]
+spisok = input()
+delimetr = " "
+a = []
+a.append(spisok)
+a = delimetr.join(a)
+a = a.split(delimetr)
+kolvo_man = 0
+kolvo_woman = 0
+dlina = 0
+okonch = ""
+const = 4
+neconst = 0
+vrem = 0
 
-spisok_imen = ["Emily Hannah Kaitlyn Sarah Madison Brianna Kaylee Hailey Alexis Elizabeth Taylo Lauren Ashley Katherine Jessica Anna Samantha lissa Kayla"]
-delimiter = " "
-spisok_imen = delimiter.join(spisok_imen)
-spisok_imen = spisok_imen.split(delimiter)
-
-group_1 = []
-group_2 = []
-group_3 = []
-Number_of_students = 20
-Number_of_names = len(spisok_imen)
-count_teska = 0
-group_bez_tesok = []
-group_s_teskami = []
-
-
-for i in range(Number_of_students):
-    sequence_number_name = randint(0,Number_of_names-1)
-    group_1.append(spisok_imen[sequence_number_name])
-
-    sequence_number_name = randint(0, Number_of_names - 1)
-    group_2.append(spisok_imen[sequence_number_name])
-
-    sequence_number_name = randint(0, Number_of_names - 1)
-    group_3.append(spisok_imen[sequence_number_name])
-
-print("Первая группа",group_1)
-print("Вторая группа",group_2)
-print("Третья группа",group_3)
+for i in a:
+    dlina = len(i)
+    for j in range(dlina-1,0-1,-1):
+        if neconst != 4:
+            okonch += i[j]
+            neconst += 1
+    okonch = okonch[::-1]
 
 
-for qew in range(Number_of_students):
-    name_student = group_1[qew]
-    count_teska = group_2.count(group_1[qew]) + group_3.count(group_1[qew])
-    if count_teska == 0:
-        group_bez_tesok.append(name_student)
-    count_teska = group_2.count(group_1[qew]) * group_3.count(group_1[qew])
-    if count_teska > 0:
-        group_s_teskami.append(name_student)
+    for m in range(len(woman)):
+        perv = woman[m]
+        vtor = okonch
+        dlina = len(perv)
+        counter = 0
 
-    name_student = group_2[qew]
-    count_teska = group_1.count(group_1[qew]) + group_3.count(group_1[qew])
-    if count_teska == 0:
-        group_bez_tesok.append(name_student)
+        for i in vtor:
+            if i == perv[counter]:
+                counter += 1
 
-    name_student = group_3[qew]
-    count_teska = group_1.count(group_1[qew]) + group_2.count(group_1[qew])
-    if count_teska == 0:
-        group_bez_tesok.append(name_student)
+            else:
+                counter = 0
+
+            if counter == dlina:
+                kolvo_woman +=1
+                counter = 0
+                vrem= 1
+    if vrem == 0:
+        for n in range(len(man)):
+            perv = man[n]
+            vtor = okonch
+            kolvo = 0
+            dlina = len(perv)
+            counter = 0
+
+            for i in vtor:
+                if i == perv[counter]:
+                    counter += 1
+
+                else:
+                    counter = 0
+
+                if counter == dlina:
+                    kolvo_man += 1
+                    counter = 0
+    vrem = 0
 
 
-print(" ")
-print("Студенты с тесками:  ",group_s_teskami)
-print("Студенты без тесок:  ",group_bez_tesok)
+
+
+
+    neconst = 0
+    okonch = ""
+
+print(kolvo_woman)
+print(kolvo_man)
