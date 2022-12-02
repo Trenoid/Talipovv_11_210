884fb120558dd1029155cb49e30cb954a9f91134
Trenoid <112693010+Trenoid@users.noreply.github.com>
2022-12-03 02:30:28 +0500

    Update 1zad.py

--- 1zad.py
+++ 1zad.py
@@ -1,28 +1,54 @@
-from random import randint
-chislo=0
-spisok=[]
-counter = 0
-allcounter = 0
-while chislo !="q":
-	chislo=input("Введите число:  ")
-	if chislo== "q":
-		break
-	randomchislo=randint(1,9)
-	
-	if (int(chislo) >0) and (int(chislo) <10):
-		if int(chislo)==randomchislo:
-			print("Угадано загаданное число -",randomchislo)
-			counter +=1
-		else:
-			print("Не угадано загаданое число -",randomchislo)
-		spisok.append(randomchislo)
-		print("Список загаданных чисел -"," ".join(map(str,spisok)))
-		allcounter+=1
-		ugadano=int(100*counter/allcounter)
-		neugadano=int(100*(allcounter-counter)/allcounter)
-		print("Статистика угадано/не угадано:",ugadano,"%",neugadano,"%")
-		print("")
-		print("")
-	
-	else:
-		print("Введите число от 1 до 9")
+class student:
+    plata_obuchenie = 80000 # Стоимость обучения
+    balance = 0             # Баланс на карте
+    grades = []             # Оценки студента
+
+    def __init__ (self,name,group):
+        self.name = name    # Имя студента
+        self.group = group      # номер группы
+
+    def deposit (self,amount):      #  <экземпляр>.deposit(x) пополнит баланс карты на x
+        self.balance = self.balance + amount
+        return self.balance
+
+    def withdraw (self,amount):         #  <экземпляр>.withdraw(x) снимет с  баланса карты  x
+        if amount > self.balance:       # Проверяет достаточно ли средств на карте
+            return "Недостаточно средств"       # Выводит ошибку если на карте недостаточно средств
+        self.balance = self.balance - amount
+        return self.balance
+
+    def perfomance(self,grade):     # успеваемость студента: <экз>.perfomance(x) добовляет в список оценку x, x == [1,5]
+        self.grade = grade
+        self.grades.append(self.grade)      # Добовляет оценку от 1 до 5 в список оценок
+        return self.grades
+
+
+
+class contract(student):        # Новый подкласс, с наследованием от student. Предназначен для студентов контрактников
+    plata_obuchenie = 80000     # Фиксированная плата за обучение
+    status = "Не оплачено"
+
+    def oplata(self):      # <экземпляр>.oplata() снимает с баланса plata_obuchenie и меняет статус на "Оплачено"
+        if self.balance < self.plata_obuchenie:     # Проверяет достаточно ли средств на балансе карты
+            return "Недостаточно средств"
+        self.balance = self.balance - self.plata_obuchenie
+        self.status = "Оплачено"
+        return self.balance
+
+    def trancition(self):       # Заявка на переход с контрактного на бюджетное обучение
+        for counter in self.grades:     # Начинает проверку успеваемости
+            if counter != 5:            # Если студент не отличник отклоняет заявку на бюджетное обучение
+                return "Вы не можете перейти на бюджет из-за недостаточной успеваемости"
+        return "Мы расмотрим ваш переход на бюджет"
+
+
+acc1 = contract("Ramis","11_210")
+acc1.deposit(100000)
+acc1.withdraw(300)
+acc1.oplata()
+print(acc1.balance)
+
+acc1.perfomance(5)
+acc1.perfomance(5)
+print(acc1.grades)
+print(acc1.trancition())
