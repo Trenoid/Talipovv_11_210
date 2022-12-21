353f9ec3d7e22e640d13284c78f3a7ef8fdf4918
Trenoid <112693010+Trenoid@users.noreply.github.com>
2022-12-21 18:57:53 +0500

    Update 1zad.py

--- 1zad.py
+++ 1zad.py
@@ -1,28 +1,31 @@
-from random import randint
-chislo=0
-spisok=[]
-counter = 0
-allcounter = 0
-while chislo !="q":
-	chislo=input("Введите число:  ")
-	if chislo== "q":
-		break
-	randomchislo=randint(1,9)
-	
-	if (int(chislo) >0) and (int(chislo) <10):
-		if int(chislo)==randomchislo:
-			print("Угадано загаданное число -",randomchislo)
-			counter +=1
-		else:
-			print("Не угадано загаданое число -",randomchislo)
-		spisok.append(randomchislo)
-		print("Список загаданных чисел -"," ".join(map(str,spisok)))
-		allcounter+=1
-		ugadano=int(100*counter/allcounter)
-		neugadano=int(100*(allcounter-counter)/allcounter)
-		print("Статистика угадано/не угадано:",ugadano,"%",neugadano,"%")
-		print("")
-		print("")
-	
-	else:
-		print("Введите число от 1 до 9")
+class poiskovik:
+
+    def __init__(self,massiv):
+        self.massiv = massiv
+        self.UporMassiv = sorted(self.massiv)
+        self.ForPoiskUporMassiv = self.UporMassiv
+
+    def poisk(self,number):
+        self.dlina = len(self.ForPoiskUporMassiv)
+        self.middle = self.dlina // 2
+        self.averageNumber = self.ForPoiskUporMassiv[self.middle]
+
+        if self.averageNumber != number:
+            if self.averageNumber > number:
+                self.ForPoiskUporMassiv = self.ForPoiskUporMassiv[0:self.middle]
+                self.poisk(number)
+
+            if self.averageNumber < number:
+                self.ForPoiskUporMassiv = self.ForPoiskUporMassiv[self.middle:]
+                self.poisk(number)
+
+        if self.averageNumber == number:
+            return self.UporMassiv.index(number)
+
+
+
+
+
+a = poiskovik([4,12,15,17,54,34,23,36,78,45,65,39,21,56])
+print(a.UporMassiv)
+print(f"{a.poisk(34)} ---index, {a.poisk(34)+1}--- Порядковый номер ")
